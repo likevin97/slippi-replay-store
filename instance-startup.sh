752c38939d29cdb8f3b260a346bdba77f54b8f0e
Kevin Li <likevin0718@gmail.com>
2022-04-26 16:12:04 -0400

    WIP. Need to fix setting of external IP.

--- instance-startup.sh
+++ instance-startup.sh
@@ -9,6 +9,17 @@ echo "Project ID: ${PROJECTID} Bucket: ${BUCKET}"
 # Get the files we need
 gsutil cp gs://${BUCKET}/*.jar .
 
+# Set current public IP to domain
+IP=$(curl -s api.ipify.org) 
+gcloud dns record-sets delete www.slippi-replay.store. \
+    --type=A \
+    --zone=slippi-replay-store-zone
+gcloud dns record-sets create www.slippi-replay.store. \
+    --rrdatas=$IP \
+    --ttl=TTL \
+    --type=A \
+    --zone=slippi-replay-store-zone
+
 # Install dependencies
 apt-get update
 apt-get -y --force-yes install openjdk-11-jdk
@@ -17,4 +28,4 @@ apt-get -y --force-yes install openjdk-11-jdk
 update-alternatives --set java /usr/lib/jvm/java-11-openjdk-amd64/jre/bin/java
 
 # Start server
-java -jar slippi-replay-store-0.0.1-SNAPSHOT.jar
+authbind --deep java -jar slippi-replay-store-0.0.1-SNAPSHOT.jar
--- src/main/java/com/slippi/replays/WebSecurityConfig.java
+++ src/main/java/com/slippi/replays/WebSecurityConfig.java
@@ -3,15 +3,15 @@ package com.slippi.replays;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
-            .antMatchers("/", "/error", "/user/signup", "/user/signin", "/swagger-ui.html", "/.well-known/acme-challenge")
-            .permitAll().anyRequest().authenticated().and().sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http
+            .anonymous().and()
+            .authorizeRequests()
+                .antMatchers("/.well-known/acme-challenge/*")
+                .permitAll();
     }
 }
